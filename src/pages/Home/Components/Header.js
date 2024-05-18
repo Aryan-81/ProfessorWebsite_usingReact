9f6a3c1633555efa83633ddebf72e8c21164f950
Aryan-81 <131597580+Aryan-81@users.noreply.github.com>
2024-05-18 10:05:23 +0530

    correcting the style of home page

--- src/pages/Home/Components/Header.js
+++ src/pages/Home/Components/Header.js
@@ -6,16 +6,17 @@ import bond_svg from '../../../assets/picsvg_download.svg'
 
 function Header(){
   return (
-    <div className={styles['img']} style={{ backgroundImage: `url('${bk_img}')` }}>
+    <div className={styles['imgContainer']} style={{ backgroundImage: `url('${bk_img}')` }}>
       <div className={styles['bluebox']}>
-        <div className={styles['heading']}>
+        <div className={styles['detail']}>
+          <div>
           <img className={styles['bond']} src={bond_svg} width="65px" alt="Avatar" />
           <h1>Dr. Yogesh Madhukarrao Nimdeo</h1>
           <h2>Assistant Professor</h2>
           <h2>HOD of Chemical Engineering Department <br/>At IIT Jammu</h2>
           <div className={styles['line']}></div>
           <h3>EMAIL:</h3>
-          <a id="email" className={styles['.email']} href="mailto:yogesh.nimdeo@iitjammu.ac.in">
+          <a id="email" className = {styles['email']}  href="mailto:yogesh.nimdeo@iitjammu.ac.in">
             <h4>yogesh.nimdeo@iitjammu.ac.in</h4>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" id={styles['link']}>
               <path fill="#979797" d="M18.51 3.868a4 4 0 0 0-5.657 0L8.611 8.111a4 4 0 0 0 5.657 5.657l.502-.503s-.226-.635-.502-.911c-.276-.277-.912-.503-.912-.503l-.502.503a2 2 0 0 1-2.829-2.829l4.243-4.243a2 2 0 0 1 2.828 2.829l-.707.707s.49.318.798.627c.31.309.616.787.616.787l.707-.707a4 4 0 0 0 0-5.657Z"></path>
@@ -25,6 +26,7 @@ function Header(){
           <div className={styles['line']}></div>
           <h3>TELEPHONE No. :</h3>
           <h4>123-321</h4>
+          </div>
         </div>
       </div>
     </div>
